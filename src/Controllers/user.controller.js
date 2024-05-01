28ab3affcfb017cb65e16632c86b172ee958b99d
Anil Pal <anilcodes01@gmail.com>
2024-05-01 14:50:08 +0530

    add register controller

--- src/Controllers/user.controller.js
+++ src/Controllers/user.controller.js
@@ -1,7 +1,8 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { User } from "../models/user.models.js";
-import { uploadOnCloudinary} from '../utils/Cloudinary.js'
+import { uploadOnCloudinary } from "../utils/Cloudinary.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
 const registerUser = asyncHandler(async (req, res) => {
   // get user details from frontend
@@ -37,23 +38,31 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new ApiError(400, "Avatar file is required!");
   }
 
-   const avatar = await uploadOnCloudinary(avatarLocalPath)
-   const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath);
 
-   if(!avatar) {
-    throw new ApiError(400, 'Avatar file is required!')
-   }
+  if (!avatar) {
+    throw new ApiError(400, "Avatar file is required!");
+  }
 
-  
-
-   User.create({
+  const user = await User.create({
     fullName,
     avatar: avatar.url,
-    coverImage: coverImage?.url || '',
+    coverImage: coverImage?.url || "",
     email,
     password,
-    username: username.toLowerCase()
-   })
+    username: username.toLowerCase(),
+  });
+
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+  if (!createdUser) {
+    throw new ApiError(500, "Something went wrong while registering the user!");
+  }
+return res.status(201).json(
+    new ApiResponse(200, createdUser, 'User registered successfully!' )
+)
 
 });
 
