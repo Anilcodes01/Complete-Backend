c25c917bec0f130c58755d11c7d8a2c7f95ae286
Anil Pal <anilcodes01@gmail.com>
2024-05-01 15:54:02 +0530

    testing and debugging

--- src/Controllers/user.controller.js
+++ src/Controllers/user.controller.js
@@ -23,7 +23,7 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new ApiError(400, "All fields are required!");
   }
 
-  const existingUser = User.findOne({
+  const existingUser = await User.findOne({
     $or: [{ username }, { email }],
   });
 
@@ -31,8 +31,14 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new ApiError(409, "User with this username or email already exists!");
   }
 
+  console.log(req.files);
   const avatarLocalPath = req.files?.avatar[0]?.path;
-  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+//   const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+  let coverImageLocalPath;
+  if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0){
+         coverImageLocalPath = req.files.coverImage[0].path
+  }
 
   if (!avatarLocalPath) {
     throw new ApiError(400, "Avatar file is required!");
